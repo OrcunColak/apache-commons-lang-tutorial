4861b5b98ec8b03190eb3fa96b89eada2eee5ac0
Orçun Çolak <orcun.colak@hazelcast.com>
2025-05-15 21:31:32 +0300

    more dev

--- src/main/java/com/colak/SystemUtilsTest.java
+++ src/main/java/com/colak/SystemUtilsTest.java
@@ -9,11 +9,11 @@ class SystemUtilsTest {
     public static void main(String[] args) {
         // Get the Java version  
         String javaVersion = SystemUtils.JAVA_VERSION;
-        log.info("Java Version: " + javaVersion);
+        log.info("Java Version: {}", javaVersion);
 
         // Check if the OS is Windows  
         boolean isWindows = SystemUtils.IS_OS_WINDOWS;
-        log.info("Is Windows: " + isWindows);
+        log.info("Is Windows: {}", isWindows);
 
     }
 }
